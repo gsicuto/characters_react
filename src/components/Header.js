cf50e8d34046ca0b9bd48cb481ee0910bfbfbf9a
Gabriel Sicuto <gabrielsicuto@gmail.com>
2021-08-03 20:51:35 -0300

    include link to home path on header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import{ Link } from 'react-router-dom'
 const Header = () => {
   return (
     <nav>
-      <img src='' alt='logo'/>
+      <Link to= '/'><img src='' alt='logo'/></Link>
       <Link to='/characters'>Characters</Link>
       <Link to='/new-character'>New Character</Link>
     </nav>
