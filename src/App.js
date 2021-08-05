c927ec4c4aa41996db1dba9c942715cd9859999e
Gabriel Sicuto <gabrielsicuto@gmail.com>
2021-08-05 20:03:00 -0300

    created Home Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
       <Header/>
+      <Switch>
+        <Route exact path='/'component={Home}/>
+      </Switch>
       <Footer/>
     </>
   );
