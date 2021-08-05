ca31dc03ba8b06c7ab41c7e8439759d751fce350
Gabriel Sicuto <gabrielsicuto@gmail.com>
2021-08-05 20:51:46 -0300

    Change App for class component and fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import { Switch, Route } from 'react-router-dom';
-import './App.css';
-import CharacterCard from './components/CharacterCard';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Home from './components/Home';
+import { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import axios from 'axios';
 
-function App() {
-  return (
-    <>
-      <Header/>
-      <Switch>
-        <Route exact path='/'component={Home}/>
-      </Switch>
-      <Footer/>
-    </>
-  );
+import "./App.css";
+
+import CharacterCard from "./components/CharacterCard";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+
+
+class App extends Component {
+  
+  state = {
+    charactersList: []
+  } 
+
+  componentDidMount = () => {
+    console.log(process.env)
+    axios.get(`http://localhost:8000/characters`)
+      .then((result)=>{
+        this.setState({
+          charactersList: result.data
+        })
+      })
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
